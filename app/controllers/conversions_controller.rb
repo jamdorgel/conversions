ed5160e54dc3991253c1eb3858f3aafe1bd4ab82
jamdorothy <jdgelua@gmail.com>
2021-12-14 12:15:42 -0500

    Write service, controller index method, index page

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,5 +1,6 @@
 class ConversionsController < ApplicationController
     def index
-        
+        conversion_service = ConversionService.new
+        @currency_codes = conversion_service.currency_codes
     end
 end
